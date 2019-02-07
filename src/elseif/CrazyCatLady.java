b5c3ba574625b3e914f4a5f737df237c1b392bf1
demiansebat <league@WorthyMini-6.attlocal.net>
2019-02-06 16:55:01 -0800

    Robot Color Chooser, Crazy cat lady

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,19 +5,28 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String yay= JOptionPane.showInputDialog(null, "How many cats do you have?");
 		// 2. Convert their answer into an int
-
+int number= Integer.parseInt(yay);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(number>=3){
+	JOptionPane.showMessageDialog(null, "You are a CrAzY cAt LaDy!!!!!!!!!!");}
+		// 4. If they have less than 3 cats AND more than 0 cats, call the method below
+		// to show them a cat video
+if(number<3&&number >0){
+	playVideo("https://www.youtube.com/watch?v=g7ZnOq-o7rc");
+}
+		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a
+		// Human
+else if(number==0) {
+	playVideo("https://www.youtube.com/watch?v=W_juM14WHNQ");}
 
-		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
-		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-	}
+}
 
 	static void playVideo(String videoURL) {
 		try {
@@ -29,4 +38,3 @@ public class CrazyCatLady {
 	}
 
 }
-
