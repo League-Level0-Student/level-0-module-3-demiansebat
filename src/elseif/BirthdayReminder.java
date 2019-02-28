4da0f5d81f5a381537f6b4c258ecc46bc2e754d9
mi <League@AdministorsiMac.attlocal.net>
2019-02-27 16:59:01 -0800

    Horoscope, Birthday Reminder IP

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,16 +11,19 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "August 21st";
+		String dadsBirthday = "June 14th";
+		String myBirthday = "August 9th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String answer=JOptionPane.showInputDialog("What Birthday do yout want?");
 		// 3. Print out what the user typed
-		
+	System.out.println(answer);
 		// 4. if user asked for "mom"
+	if (answer.equalsIgnoreCase("Aries")) {
+		JOptionPane.showMessageDialog(null,"August 21st" );
 			//print mom's birthday
+	}
 		// 5. if user asked for "dad"
 			// print dad's birthday
 		// 6. if user asked for your name
--- src/elseif/Horoscope.java
+++ src/elseif/Horoscope.java
@@ -7,9 +7,7 @@ public class Horoscope {
 		String tell = JOptionPane.showInputDialog("What is your horoscope!?");
 		if (tell.equalsIgnoreCase("Aries")) {
 			JOptionPane.showMessageDialog(null, "Courageous, energetic, willful, commanding, leading");
-
 		}
-
 		else if (tell.equalsIgnoreCase("Taurus")) {
 			JOptionPane.showMessageDialog(null,
 					"Pleasure seeking, loves control, dependable, grounded, provokes slowly,sensual in nature.");
@@ -18,17 +16,34 @@ public class Horoscope {
 					"Cerebral, chatty, loves learning and education, charming, and adventurous.");
 		} else if (tell.equalsIgnoreCase("Cancer")) {
 		
-		JOptionPane.showMessageDialog(null, "Emotional, group oriented, seeks security, family.\n" + "");
+		JOptionPane.showMessageDialog(null, "Emotional, group oriented, seeks security, family");
 	}
 	else if (tell.equalsIgnoreCase("Leo")) {
 		JOptionPane.showMessageDialog(null,"Generous, organized, protective, beautiful.");
 	}
 	else if (tell.equalsIgnoreCase("Virgo")) {
-		JOptionPane.showMessageDialog(null,"Particular, logical, practical, sense of duty, critical");
+		JOptionPane.showMessageDialog(null,"Particular, logical, practical, sense of duty, critical.");
 	}
 	else if (tell.equalsIgnoreCase("Libra")) {
-		JOptionPane.showMessageDialog(null,"Balanced, seeks beauty, sense of justice.\n" + 
-				"");
+		JOptionPane.showMessageDialog(null,"Balanced, seeks beauty, sense of justice.");
 	}
-		
-	}}
+	else if (tell.equalsIgnoreCase("Scorpio")){
+		JOptionPane.showMessageDialog(null, "Passionate, exacting, loves extremes, combative, reflective.");
+	}
+	else if (tell.equalsIgnoreCase("Sagittarius")){
+		JOptionPane.showMessageDialog(null, "Happy, absent minded, creative, adventurous.");
+	}
+	else if (tell.equalsIgnoreCase("Capricorn")){
+		JOptionPane.showMessageDialog(null, "Timeless, driven, calculating, ambitious.");	
+	}
+	else if (tell.equalsIgnoreCase("Aquarius")){
+		JOptionPane.showMessageDialog(null, "Forward thinking, communicative, people oriented, stubborn, generous, and dedicated.");	
+	}
+	else if (tell.equalsIgnoreCase("Pisces")){
+		JOptionPane.showMessageDialog(null, "Likeable, energetic, passionate, sensitive.");	
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "That's not a horoscope STUPID...");
+	}
+	}
+	}
